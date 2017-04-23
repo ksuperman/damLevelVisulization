f549a099f3958c8f578f6667d973175bfbe51fe2
jagrutipatil <jagrutipatil32@gmail.com>
2017-04-23 14:21:33 -0700

    DB quries

--- routes/db.js
+++ routes/db.js
@@ -1,26 +1,33 @@
 var express = require('express');
 var router = express.Router();
-var resv = require('./model');
+var resv = require('./Resvmodel');
 /* GET home page. */
 
-router.get('/getAllResv', function(req, res, next) {
+router.get('/insertResv', function(req, res, next) {
 	console.log("/getAllResv");
 	
-	var resvInstance = new resv({
-	    resv_name: "dummy",
-	    resv_lat: "11.1",
-	    resv_lng: "12.2",
-	    resv_maxCap: 5000
-	    })
+		var resvInstance = new resv({
+		    resv_name: "dummy1",
+		    resv_lat: "11.2",
+		    resv_lng: "12.2",
+		    resv_maxCap: 5000
+		    })
+		
+		resvInstance.save(function (err) {
+	        if (err) {
+	        	res.send("error");
+	        } else {
+	        	res.send("success");
+	        }
+	    });
 	
-	resvInstance.save(function (err) {
-        if (err) {
-        	res.send("error");
-        } else {
-        	res.send("success");
-        }
+});
+
+router.get('/getAllResv', function(req, res, next) {
+	console.log("/getAllResv");	
+	resv.find({}, function (err, document) {
+        res.send(document);
     });
-	
 });
 
 module.exports = router;
